54df21a0537e9e290d5a2a5a2782cacb4cdc1ebe
AdrianParejaM <adrianpareja.alu@iespacomolla.es>
2024-09-18 11:57:30 +0200

    Prueba si funciona desde casa

--- Practica2.01APM/Ejercicio2.js
+++ Practica2.01APM/Ejercicio2.js
@@ -1,3 +1,24 @@
 "use strict";
 
-console.log("Hola");
+//Almacenamos los puntos de los equipos de Juan y Miguel.
+
+var equipoJuan = [89, 120, 103];
+
+var equipoMiguel = [116, 94, 123];
+
+//Función para calcular la media de los puntos.
+
+function mediaPuntos (equipo1) {
+
+    let acumulador = 0;
+
+    for (let i = 0; i < equipo1.length; index++) {
+        acumulador = equipo1[index];
+        
+    }
+
+    return acumulador / equipo1.length;
+
+}
+
+console.log(mediaPuntos(equipoJuan));
