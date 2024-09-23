a49f8ca7eb649f119bbbf543ef1a18abc6b0a81e
Adrian Pareja <adrianpareja.alu@iespacomolla.es>
2024-09-23 20:33:15 +0200

    Revisar Ejercicio 1 y acabar ejercicio 3

--- Tema 2/Practica2.02APM/Ejercicio1.js
+++ Tema 2/Practica2.02APM/Ejercicio1.js
@@ -1,3 +1,34 @@
 "use strict";
 
-console.log("Ejercicio 1");
+//Creamos la función para sumar los números introducidos por el usuario.
+
+function sumarNumeros() {
+
+    //Comprobamos si son números.
+    if (!isNaN(arguments) || arguments.length <= 2) {
+
+        //Si es así entramos en el "for".
+
+        //Antes de entrar al for creamos la variable contador.
+        contador = 0;
+        for (let i = 0; i < arguments.length; i++) {
+
+            contador += arguments[i];
+            
+        }
+
+        console.log(`La suma total es ${contador}`);
+        
+    } else {
+
+        //Si no son números o son menos de 2 números, salta el mensaje de error.
+        console.log("Error, pon sólo números y más de 2.");
+        
+    }
+
+}
+
+//Hacemos pruebas.
+
+console.log(sumarNumeros(2, 3, "hola", 4));  
+console.log(sumarNumeros(5, 5, 3, 2));
--- Tema 2/Practica2.02APM/Ejercicio3.js
+++ Tema 2/Practica2.02APM/Ejercicio3.js
@@ -1,3 +1,29 @@
 "use strict";
 
-console.log("Ejercicio 3");
+//Creamos la función para generar un array aleatorio.
+function generarArrayRandom(cantidadNumeros){
+
+    //Creamos el array vacío.
+    let arrayRandom = [];
+
+    for (let i = 0; i < cantidadNumeros; i++) {
+        
+        /*Asignamos a cada posición del array un número random hasta 
+        que llegue a la cantidad de numeros indicada por el usuario.*/
+        arrayRandom[i] =  Math.floor(Math.random()*101);
+        
+    }
+
+    /*Comprobación de que generaba los números random.
+    console.log(arrayRandom);
+    */
+
+}
+
+//Creamos la función para que sume los 2 arrays recibidos.
+function sumaDosArrays(arrayASumar1, arrayASumar2){
+
+    
+}
+
+generarArrayRandom(8);
