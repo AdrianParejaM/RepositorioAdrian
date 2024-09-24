5e1a363c67d217f43509549ca80ee2c17e0ae53e
AdrianParejaM <adrianpareja.alu@iespacomolla.es>
2024-09-24 11:39:04 +0200

    Acabados ejercicios 1 y 3

--- Tema 2/Practica2.02APM/Ejercicio1.js
+++ Tema 2/Practica2.02APM/Ejercicio1.js
@@ -4,13 +4,30 @@
 
 function sumarNumeros() {
 
-    //Comprobamos si son números.
-    if (!isNaN(arguments) || arguments.length <= 2) {
+    //Comprobamos si son más de 2 números.
+    if (arguments.length < 2) {
 
-        //Si es así entramos en el "for".
+        console.log("ERROR. Debes ingresar más de 2 números.");
+        return;
+
+    }
+
+    //Ahora comprobamos si lo que ha introducido el usuario son números.
+    for (let i = 0; i < arguments.length; i++) {
+        
+        if (isNaN(arguments[i])) {
+
+            console.log(`ERROR. El valor ${arguments[i]} no es un número válido.`);
+            return;
+            
+        }
+        
+    }
+
+        //Si todo está correcto entramos en el "for" y hacemos la suma de todos los números.
 
         //Antes de entrar al for creamos la variable contador.
-        contador = 0;
+        let contador = 0;
         for (let i = 0; i < arguments.length; i++) {
 
             contador += arguments[i];
@@ -18,17 +35,16 @@ function sumarNumeros() {
         }
 
         console.log(`La suma total es ${contador}`);
-        
-    } else {
-
-        //Si no son números o son menos de 2 números, salta el mensaje de error.
-        console.log("Error, pon sólo números y más de 2.");
-        
-    }
 
 }
 
 //Hacemos pruebas.
 
+//Prueba para que de error con el hola.
 console.log(sumarNumeros(2, 3, "hola", 4));  
+
+//Prueba correcta.
 console.log(sumarNumeros(5, 5, 3, 2));
+
+//Prueba para que de error ya que es menos de 2 números.
+console.log(sumarNumeros(5));
--- Tema 2/Practica2.02APM/Ejercicio3.js
+++ Tema 2/Practica2.02APM/Ejercicio3.js
@@ -14,16 +14,62 @@ function generarArrayRandom(cantidadNumeros){
         
     }
 
-    /*Comprobación de que generaba los números random.
-    console.log(arrayRandom);
-    */
+    return arrayRandom;
 
 }
 
 //Creamos la función para que sume los 2 arrays recibidos.
 function sumaDosArrays(arrayASumar1, arrayASumar2){
 
+    let resultadoSuma = [];
+
+    for (let i = 0; i < arrayASumar1.length; i++) {
+        
+        //Sumamos la primera posición del primer array a la última posición del segundo array.
+        resultadoSuma[i] = arrayASumar1[i] + arrayASumar2[arrayASumar2.length - 1 - i];
+
+    }
+
+    return resultadoSuma;
     
 }
 
-generarArrayRandom(8);
+//Creamos la función para imprimir el array.
+
+function imprimirArray(array) {
+
+    let resultado = "El array es: ";
+
+    for (let i = 0; i < array.length; i++) {
+        
+        resultado += `${array[i]} `;
+        
+    }
+
+    console.log(resultado);
+
+}
+
+//Hacemos la función calcular.
+function calcular(funcion1, funcion2, funcion3) {
+
+    //Genera 2 arrays con 4 números aleatorios.
+    let arrayPrueba1 = funcion1(4);
+    let arrayPrueba2 = funcion1(4);
+
+    //Suma los 2 arrays.
+    let arrayPruebaSuma = funcion2(arrayPrueba1, arrayPrueba2);
+
+    //Imprimimos el array con su función;
+    funcion3(arrayPrueba1);
+    funcion3(arrayPrueba2);
+    funcion3(arrayPruebaSuma);
+
+}
+
+//Llamamos a la función calcular.
+
+calcular(generarArrayRandom, sumaDosArrays, imprimirArray);
+
+
+
