a320fb8fc3a33e209f2ee50255bd627f97c68eff
AdrianParejaM <adrianpareja.alu@iespacomolla.es>
2024-09-29 17:02:54 +0200

    Ejercicios 1 y 3 acabados.

--- Tema 2/Practica2.04APM/Biblioteca/funcionesCuartaPractica.js
+++ Tema 2/Practica2.04APM/Biblioteca/funcionesCuartaPractica.js
@@ -2,12 +2,28 @@
 
 //Ejercicio 1.
 
+//Creamos el array con los 5 nombres(he puesto el array aquí porque si no, nombresConId no podría hacerlo aquí).
+let nombres = ["Adrián Pareja", "Antonio Recio", "Vicente Maroto", "Fermín Trujillo", "Amador Rivas"];
+
 //Creamos la función para pasar a mayúscula.
 function pasarMayusculas (valor) {
 
     return valor.toUpperCase();
 
 }
+
+//Creamos el id con el índice de cada posición y el nombre con el valor de cada posición.
+let nombresConId = nombres.map((nombres, indice) => {
+
+  return {
+
+    id: indice,
+    nombre: nombres
+
+  };
+
+});
+
 
 //Ejercicio 2.
 
@@ -42,8 +58,10 @@ function mostrarNumerosMayorCinco (array1, array2, array3) {
 
 }
 
+
 //Ejercicio 3.
 
+//Constante que nos has proporcionado.
 const usuarios = [
     {
       nombre: "Luis",
@@ -138,7 +156,102 @@ const usuarios = [
     },
   ];
 
+//Función para insertar un nuevo usuario(muestra el nombre).
+function insertarNuevoUsuario(nuevoUsuario){
+
+  let usuariosActualizados = [...usuarios, nuevoUsuario];
+  let usuariosNuevos = usuariosActualizados.map(usuario => usuario.nombre);
+  console.log(`El array actualizado es: ${usuariosNuevos}`);
+  return usuariosNuevos;
+}
+
+//Función para filtrar usuarios mayores de edad(muestra el nombre).
+function usuariosMayoresEdad(){
+
+  let mayores = usuarios.filter(usuario => usuario.preferencias.edad > 18);
+  let nombreUsuariosMayores = mayores.map(usuario => usuario.nombre);
+  console.log(`Los usuarios mayores de edad son: ${nombreUsuariosMayores}`);
+  return nombreUsuariosMayores;
+
+}
+
+//Función que devuelve el correo electrónico de Yahoo(muestra el nombre).
+function usuariosCorreoYahoo(){
+
+  let usuariosYahoo = usuarios.filter(usuario => usuario.contacto.correoelectronico.endsWith("@yahoo.com"));
+  let nombreUsuariosYahoo = usuariosYahoo.map(usuario => usuario.nombre);
+  console.log(`Usuarios con el correo Yahoo: ${nombreUsuariosYahoo}`);
+  return nombreUsuariosYahoo;
+
+}
+
+//Función para devolver los usuarios que prefieren el tema claro, mayores de edad y españoles(muestra el nombre).
+function usuariosTemaClaro(){
+
+  let usuariosFiltrados = usuarios.filter(usuario =>
+
+    usuario.preferencias.tema === "claro" &&
+    usuario.preferencias.edad >= 18 &&
+    usuario.contacto.direccion.pais === "España"
+
+  );
+
+  if (usuariosFiltrados.length == 0) {
+    
+    console.log(`No hay ningún usuario que cumpla los requisitos.`);
+    return usuariosFiltrados;
+
+  }
+  let nombreUsuariosClaro = usuariosFiltrados.map(usuario => usuario.nombre);
+  console.log(`Los usuarios con el tema claro, mayores de edad y que viven en España son: ${nombreUsuariosClaro}`);
+  return nombreUsuariosClaro;
+
+}
+
+//Función para mostrar usuarios que le faltan información(muestra el nombre).
+function usuariosFaltaInformacion(){
+
+  let usuariosIncompletos = usuarios.filter(usuario => {
+    let { nombre, preferencias, contacto } = usuario;
+    return !nombre || !preferencias || !contacto || !contacto.direccion || !contacto.correoelectronico || !contacto.telefono;
+  });
+
+  let nombreUsuariosInfo = usuariosIncompletos.map(usuario => usuario.nombre);
+  console.log("Usuarios con datos faltantes:", nombreUsuariosInfo);
+  return nombreUsuariosInfo;
+
+}
+
+//Función para añadir a los usuarios apellidos que el valor por defecto será "No indicado"(muestra el nombre y apellidos).
+function anyadirApellidos() {
+
+  let usuariosConApellidos = usuarios.map(usuario => ({ ...usuario, apellidos: "No indicado"}));
+  let nombreUsuariosApellidos = usuariosConApellidos.map(usuario =>`Nombre: ${usuario.nombre}, apellidos: ${usuario.apellidos}`);
+  console.log("Usuarios con los apellidos añadidos:", nombreUsuariosApellidos);
+  return nombreUsuariosApellidos;
+
+}
+
+//Función para añadir nueva entrada a dirección llamada código y el valor por defecto será "00000"(muestra el nombre y el código postal).
+function anyadirCodigo(){
+
+  let usuariosConCodigo = usuarios.map(user => ({
+    ...user,
+    contacto: {
+      ...user.contacto,
+      direccion: {
+        ...user.contacto.direccion,
+        codigo: "00000"
+      }
+    }
+  }));
+  let nombreUsuariosCodigo = usuariosConCodigo.map(usuario =>`Nombre: ${usuario.nombre}, código postal: ${usuario.contacto.direccion.codigo}`);
+  console.log("Usuarios con el código postal añadido:", nombreUsuariosCodigo);
+  return nombreUsuariosCodigo;
+
+}
 
 
 //Exportamos todo.
-export {pasarMayusculas, generarArrayAleatorio, mostrarNumerosMayorCinco, usuarios};
+export {pasarMayusculas, generarArrayAleatorio, mostrarNumerosMayorCinco, usuarios, nombresConId, nombres, insertarNuevoUsuario, 
+  usuariosMayoresEdad, usuariosCorreoYahoo, usuariosTemaClaro, usuariosFaltaInformacion, anyadirApellidos, anyadirCodigo};
--- Tema 2/Practica2.04APM/Ejercicio3.js
+++ Tema 2/Practica2.04APM/Ejercicio3.js
@@ -1,3 +1,43 @@
 "use strict";
 
-import {usuarios} from "./Biblioteca/funcionesCuartaPractica.js";
+//Importamos todo lo necesario para este ejercicio.
+import {insertarNuevoUsuario, usuarios, usuariosMayoresEdad, usuariosCorreoYahoo, usuariosTemaClaro, usuariosFaltaInformacion, anyadirApellidos, anyadirCodigo} from "./Biblioteca/funcionesCuartaPractica.js";
+
+//Insertar nuevo usuario.
+  let nuevoUsuario = {
+
+    nombre: "Chemita",
+    preferencias: { tema: "oscuro", idioma: "español", edad: 21 },
+    contacto: {
+      direccion: {
+        calle: "Calle 13, 13",
+        localidad: "Elda",
+        pais: "España",
+      },
+      correoelectronico: "nuevocorreo@gmail.com",
+      telefono: "123456789",
+    },
+  };
+
+  //Muestra los usuarios actualizados.
+  insertarNuevoUsuario(nuevoUsuario);
+
+  //Muestra los alumnos mayores de 18.
+  usuariosMayoresEdad();
+
+  //Muestra usuarios con el correo de Yahoo.
+  usuariosCorreoYahoo();
+
+  //Usuarios filtrados con tema claro, mayores de edad y españoles.
+  usuariosTemaClaro();
+
+  //Mostrar usuarios que le faltan información.
+  usuariosFaltaInformacion();
+
+  //Añadir a los usuarios apellidos que el valor por defecto será "No indicado";
+  anyadirApellidos();
+
+  //Añadir nueva entrada a dirección llamada código y el valor por defecto será "00000".
+  anyadirCodigo();
+
+
