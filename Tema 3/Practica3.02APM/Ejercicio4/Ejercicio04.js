4e0f9b97e8fcf800d7a10dfd039a93d61359876e
AdrianParejaM <adrianpareja.alu@iespacomolla.es>
2024-10-16 11:21:17 +0200

    practica 3 hecha

---
+++ Tema 3/Practica3.02APM/Ejercicio4/Ejercicio04.js
@@ -0,0 +1,6 @@
+"use strict";
+
+//Importamos todo lo necesario para este ejercicio.
+import {cambiarImagen} from "../biblioteca/biblioteca.js";
+
+setInterval(cambiarImagen, 2000);
--- Tema 3/Practica3.02APM/biblioteca/biblioteca.js
+++ Tema 3/Practica3.02APM/biblioteca/biblioteca.js
@@ -82,9 +82,39 @@ function cambiarColorParrafo() {
 
 
 
+//Ejercicio 4
+
+//Lista de enlaces de imágenes.
+var imagenes = [
+    "https://album.mediaset.es/cimg/1000001/2017/02/14/r1.jpg",
+    "https://www.moncloa.com/wp-content/uploads/2022/07/la-que-se-avecina-1.jpg",
+    "https://album.mediaset.es/cimg/1000001/2017/02/14/f1.jpg?w=480",
+    "https://es.web.img3.acsta.net/newsv7/22/03/15/15/13/2329900.jpg",
+    "https://imagenes.20minutos.es/files/image_640_auto/uploads/imagenes/2023/07/28/amador-rivas-pablo-chiapella.jpeg",
+];
+
+//Referenciamos el 'img' del HTML.
+var imagenActual = document.getElementById("imagenActual");
+
+//Creamos un índice para controlar la imagen actual.
+var indice = 0;
+
+//Función para cambiar la imagen con la transición.
+function cambiarImagen() {
+  
+  imagenActual.style.opacity = 0;
+
+  setTimeout(function() {
+      indice = (indice + 1) % imagenes.length;
+      imagenActual.src = imagenes[indice];
+
+      
+      imagenActual.style.opacity = 1;
+  }, 1000); 
+};
 
 
 
 
 //Exportamos todo.
-export {sustituirPalabra, crearTabla, resaltarPrimos, cambiarColorParrafo};
+export {sustituirPalabra, crearTabla, resaltarPrimos, cambiarColorParrafo, cambiarImagen};
