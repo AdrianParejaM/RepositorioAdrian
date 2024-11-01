de60d34233f7a24d5478363a99651a68f0f55a17
AdrianParejaM <adrianpareja.alu@iespacomolla.es>
2024-11-01 16:48:36 +0100

    practica 4.02 hecha

--- Tema 4/Practica4.02APM/Ejercicio01/Ejercicio01.js
+++ Tema 4/Practica4.02APM/Ejercicio01/Ejercicio01.js
@@ -4,14 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Aquí dentro va todo el ejercicio.
 
+    //Selecionamos los 'h3' y los 'p'.
+    const cabeceras = document.querySelectorAll('h3');
+    const subTexto = document.querySelectorAll('p');
     
+    //No lo he realizado en una biblioteca porque no he visto necesario meter este for en una función.
 
+    // Recorremos cada h3.
+    for (let i = 0; i < cabeceras.length; i++) {
+        // Agregamos el 'addEventListener' a los elementos impares.
+        if (i % 2 === 0) {
+            
+            cabeceras[i].addEventListener('click', (event) => {
+                
+                const content = subTexto[i / 2];  
 
-
-
-
-
-
+                
+                content.classList.toggle('visible');
+            }, false);
+        }
+    }
 
 
     //FIN DE DOMContentLoaded.
--- Tema 4/Practica4.02APM/Ejercicio02/Ejercicio02.js
+++ Tema 4/Practica4.02APM/Ejercicio02/Ejercicio02.js
@@ -1,1 +1,26 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Aquí dentro va todo el ejercicio.
+
+    //Al igual que el anterior no he visto necesario hacer una función solo para hacer esto.
+
+    // Seleccionamos todas las pestañas y todos los contenidos.
+    const pestanyas = document.querySelectorAll('.pestanya');
+    const contenidos = document.querySelectorAll('.contenido');
+
+    // Recorremos cada pestaña y le añadimos 'addEventListener'.
+    pestanyas.forEach((pestanya, index) => {
+        pestanya.addEventListener('click', (event) => {
+            // Para lo de 'active' me ayudó el chat.
+            contenidos.forEach(contenido => contenido.classList.remove('active'));
+
+            // Mostramos el contenido.
+            contenidos[index].classList.add('active');
+        }, false);
+    });
+
+
+    //FIN DE DOMContentLoaded.
+});
