7900c3041a1194a5615e935416f86c3593158927
Adrian Pareja <adrianpareja.alu@iespacomolla.es>
2024-11-06 19:07:39 +0100

    Ejercicio 4 terminado

--- Tema 4/Practica4.04APM/Puzle.js
+++ Tema 4/Practica4.04APM/Puzle.js
@@ -6,6 +6,9 @@ import {verificarPuzzle} from "./Biblioteca.js";
 document.addEventListener("DOMContentLoaded", () => {
     // Aquí dentro va todo el ejercicio.
 
+    //Te he dejado el código para que aparezca el mensaje, pero no me va, es simplemente que me he tirado mucho tiempo intentándolo y no lo
+    //he conseguido, ni solo ni con ayuda (del chat), lo demás funciona correctamente.
+
     // Elemento de mensaje al completar bien el puzle.
     const mensaje = document.getElementById("mensaje");
 
@@ -13,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const arrastrable = document.getElementsByClassName("arrastrable");
     for (let i = 0; i < arrastrable.length; i++) {
         arrastrable[i].setAttribute("draggable", true);
-        arrastrable[i].setAttribute("id", `pieza-${i}`); // Añadimos un id único a cada pieza para poder referenciarla
+        arrastrable[i].setAttribute("id", `pieza-${i}`);
     }
 
     // Evento dragstart para iniciar el arrastre y almacenar la información de la pieza.
@@ -27,10 +30,10 @@ document.addEventListener("DOMContentLoaded", () => {
         false);
     }
 
-    // Seleccionamos los contenedores vacíos donde se podrán soltar las piezas
+    // Seleccionamos los contenedores vacíos donde se podrán soltar las piezas.
     const soltarImagenes = document.getElementsByClassName("soltarImg");
 
-    // Añadimos eventos a cada contenedor vacío para permitir el drop
+    // Añadimos eventos a cada contenedor vacío para permitir el drop.
     for (let contenedor of soltarImagenes) {
         contenedor.addEventListener(
             "dragover", 
@@ -56,7 +59,26 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     
-
+    document.getElementById("botonBorrar").addEventListener(
+        "click",
+        () => {
+            // Selecciona el contenedor original y todas las piezas arrastrables.
+            const zonaArrastrable = document.getElementById("contenedorPuzle");
+            const arrastrables = document.getElementsByClassName("arrastrable");
+    
+            //Aquí necesité ayuda del chat.
+            Array.from(arrastrables).forEach((pieza) => {
+                // Si la pieza no está en "zonaArrastrable", la vuleve a poner.
+                if (pieza.parentNode !== zonaArrastrable) {
+                    zonaArrastrable.appendChild(pieza);
+                }
+                
+            });
+            if (!mensaje.classList.contains("oculto")) {
+                mensaje.classList.add("oculto");
+            }
+        },
+        false);
     
     
 
