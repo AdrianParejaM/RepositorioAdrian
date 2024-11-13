9fe05598b45e6b55027338944e9f03e57b04ce34
Adrian Pareja <adrianpareja.alu@iespacomolla.es>
2024-11-13 15:17:35 +0100

    faltan 3, 4 y 5

--- Tema 4/Practica4.06APM/Ejercicio2/Ejercicio2.js
+++ Tema 4/Practica4.06APM/Ejercicio2/Ejercicio2.js
@@ -4,13 +4,23 @@
 import {crearParrafo} from "../Biblioteca.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+
     // Aquí dentro va todo el ejercicio.
+
+
+    const resultadoDiv = document.getElementById("resultado");
   
+    //Creamos el evento en el botón.
     document.getElementById("crear").addEventListener("click",
         (evento) => {
         const texto = document.getElementById("texto").value;
         const estilo = document.getElementById("estilo").value;
-            crearParrafo(texto, estilo);
+        //comprobamos si el texto está vacío.
+            if (texto.trim() === '') {
+                resultadoDiv.innerHTML = "<p>Pon algo en el texto, ¡cazurro!</p>";
+            }else{
+                crearParrafo(texto, estilo);
+            }
         },
     false);
 
