d9b2710f22303e4a747bf63699657c689eab64b7
Adrian Pareja <adrianpareja.alu@iespacomolla.es>
2024-11-18 18:22:13 +0100

    ejercicio x e y del raton hecho

--- Tema 4/Practica4.01APM/Ejercicio03.js
+++ Tema 4/Practica4.01APM/Ejercicio03.js
@@ -1,1 +1,24 @@
 "use strict";
+
+//¡¡IMPORTANTE!! PONER PARA TODO A PARTIR DE AHORA.
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Aquí dentro va todo el ejercicio.
+
+    // Seleccionar el elemento donde se mostrará la posición del ratón
+    const mousePositionElement = document.getElementById('mouse-position');
+
+    document.addEventListener('mousemove', 
+        (evento) => {
+        // Obtener las coordenadas actuales del ratón
+        const x = evento.clientX;
+        const y = evento.clientY;
+
+        // Actualizar el contenido del elemento con las coordenadas
+        mousePositionElement.textContent = `X: ${x}, Y: ${y}`;
+        },
+    false);
+
+    //FIN DE DOMContentLoaded.
+});
+
