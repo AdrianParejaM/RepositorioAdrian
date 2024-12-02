bb24cf3b0d531e1a1a8f988791cc841d462291b9
Adrian Pareja <adrianpareja.alu@iespacomolla.es>
2024-12-02 15:26:25 +0100

    cambios

---
+++ Tema 5/Practica5.01APM/Ejercicio2/Ejercicio02.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Aquí dentro va todo el ejercicio.
+
+    console.log("hola");
+
+    //FIN DE DOMContentLoaded.
+});
---
+++ Tema 5/Practica5.01APM/Ejercicio3/Ejercicio03.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Aquí dentro va todo el ejercicio.
+
+    console.log("hola que tal");
+
+    //FIN DE DOMContentLoaded.
+});
