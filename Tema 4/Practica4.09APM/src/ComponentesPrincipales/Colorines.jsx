104fee80accee69e963e3b25de38016040df3345
Adrian Pareja <adrianpareja.alu@iespacomolla.es>
2024-12-03 20:16:53 +0100

    4.09 biblioteca dentro

--- Tema 4/Practica4.09APM/src/ComponentesPrincipales/Colorines.jsx
+++ Tema 4/Practica4.09APM/src/ComponentesPrincipales/Colorines.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {generarColorHexadecimal} from  "../../Biblioteca/Biblioteca.js";
+import {generarColorHexadecimal} from  "../Biblioteca/Biblioteca.js";
 
 const Colorines = () => {
 
